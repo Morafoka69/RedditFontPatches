ab7750f1ea9b61822612f287df077a0b8eecd95c
Anthrax.xx <155040570+Morafoka69@users.noreply.github.com>
2026-07-01 20:24:40 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,20 @@
 plugins {
     alias(libs.plugins.android.library) apply false
+    id("app.morphe.patches-gradle-plugin") version "1.5.0"
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/MorpheApp/registry")
+    }
+}
+
+patches {
+    about {
+        name = "Custom Reddit Typography"
+        description = "Forces system fonts and custom SP sizes on Reddit."
+        version = "1.0.0"
+    }
 }
